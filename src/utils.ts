2e243af2013e3750d7c6a2a56d526e174ede6326
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-07-28 22:54:33 +0200

    refactor(utils): remove getCalendarData function
    - Remove the `getCalendarData` function from `src/utils.ts` as it is no longer needed.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { CalendarData, CalendarType } from './app/types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -22,19 +21,6 @@ export const getSearchParams = (
   if (!param) return null
 
   return slice ? param.slice(0, slice) : param
-}
-
-export async function getCalendarData(
-  type?: CalendarType,
-): Promise<CalendarData | CalendarData[]> {
-  const response = await fetch(`/api/data/calendar`)
-  const result = (await response.json()) as { data: CalendarData[] }
-
-  if (type) {
-    return result.data.filter((d) => d.id === type)[0] as CalendarData
-  }
-
-  return result.data as CalendarData[]
 }
 
 export const createFormattedDateString = (date: Date): string => {
