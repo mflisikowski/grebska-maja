3176f150e6106f01d1c40ae323c3e0e6536a0d4b
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-07-29 13:33:01 +0200

    refactor(types): separate calendar types into their own file
    - Move `CalendarData`, `CalendarType`, and related constants to a new file `src/types/calendar.ts`.
    - Update `src/types/index.ts` to re-export from `src/types/calendar.ts`.

---
+++ src/types/calendar.ts
@@ -0,0 +1,21 @@
+export const calendarValidTypes = [
+  'psychological-consultation',
+  'individual-psychotherapy',
+]
+
+export interface CalendarData {
+  name: string
+  duration: string
+  price: string
+  id: string
+  href: string
+  description: string
+  features: string[]
+}
+
+export enum CalendarType {
+  PsychologicalConsultation = 'psychological-consultation',
+  IndividualPsychotherapy = 'individual-psychotherapy',
+}
+
+export const validTypes: CalendarType[] = Object.values(CalendarType)
