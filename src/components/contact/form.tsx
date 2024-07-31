bda30dca7dc5ebdbdbe348527817fbdc9c7cb6ae
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-07-31 11:18:45 +0200

    refactor(contact): add dynamic map and update contact form layout
    - Integrated dynamic import for the map component using `next/dynamic`.
    - Added `Loader` component as a fallback for the map.
    - Refactored contact information section to include a map drawer for the address.
    - Updated contact form layout and styles for better readability and usability.

--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -1,5 +1,14 @@
 'use client'
 
+import { Button, buttonVariants } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 import {
   Form,
   FormControl,
@@ -8,22 +17,23 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
-import { toast } from 'sonner'
-import { ContactSchema } from '@/schema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { Textarea } from '@/components/ui/textarea'
 import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { z } from 'zod'
-import { useState } from 'react'
-import { Container } from '@/components/container'
-import { createFormattedDateString } from '@/utils'
+import { Textarea } from '@/components/ui/textarea'
+import { ContactSchema } from '@/schema'
+import { cn, createFormattedDateString } from '@/utils'
 import {
   BuildingOffice2Icon,
   EnvelopeIcon,
   PhoneIcon,
 } from '@heroicons/react/24/outline'
+import { zodResolver } from '@hookform/resolvers/zod'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+import { useMemo, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+import { Loader } from '../loader'
 
 export const ContactForm = () => {
   const [loading, setLoading] = useState(false)
@@ -78,135 +88,175 @@ export const ContactForm = () => {
       })
   }
 
-  return (
-    <Container className="">
-      <div className="grid max-w-7xl grid-cols-1 space-y-24 lg:grid-cols-2 lg:gap-20 lg:space-y-0">
-        <div className="mx-auto max-w-xl space-y-14 lg:mx-0 lg:max-w-lg">
-          <div className="space-y-8">
-            <h2 className="text-balance text-left text-3xl font-bold leading-tight tracking-tight text-gray-900 sm:text-5xl">
-              Czekam na Twoje Zapytanie
-            </h2>
-
-            <p className="text-balance text-lg leading-8">
-              Masz pytania, potrzebujesz wsparcia, lub chcesz dowiedzieć się
-              więcej?
-            </p>
-
-            <p className="text-balance text-base leading-8">
-              Skontaktuj się ze mną już dziś! Wypełnij formularz kontaktowy,
-              zadzwoń lub wyślij e-mail.
-            </p>
-
-            <p className="text-balance text-base leading-8">
-              Z przyjemnością udzielę Ci odpowiedzi i pomogę znaleźć najlepsze
-              rozwiązania. Oczekuję na Twoją wiadomość!
-            </p>
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('@/components/map'), {
+        loading: () => (
+          <div className="flex h-full w-full items-center justify-center">
+            <Loader />
           </div>
+        ),
+        ssr: false,
+      }),
+    [],
+  )
 
-          <dl className="space-y-4 text-base leading-7">
-            <div className="flex gap-x-4">
-              <dt className="flex-none">
-                <span className="sr-only">Address</span>
-                <BuildingOffice2Icon aria-hidden="true" className="h-7 w-6" />
-              </dt>
-              <dd>Ul. Borówkowa 32, Rotmanka k. Pruszcza Gdańskiego</dd>
-            </div>
-            <div className="flex gap-x-4">
-              <dt className="flex-none">
-                <span className="sr-only">Telephone</span>
-                <PhoneIcon aria-hidden="true" className="h-7 w-6" />
-              </dt>
-              <dd>
-                <a href="tel:+48883937022" className="hover:text-gray-900">
-                  +48 883 937 022
-                </a>
-              </dd>
-            </div>
-            <div className="flex gap-x-4">
-              <dt className="flex-none">
-                <span className="sr-only">Email</span>
-                <EnvelopeIcon aria-hidden="true" className="h-7 w-6" />
-              </dt>
-              <dd>
-                <a
-                  href="mailto:kontakt@majagrebska.pl"
-                  className="hover:text-gray-900"
-                >
-                  kontakt@majagrebska.pl
-                </a>
-              </dd>
-            </div>
-          </dl>
+  return (
+    <div className="grid grid-cols-1 space-y-24 lg:grid-cols-2 lg:gap-20 lg:space-y-0">
+      <div className="mx-auto max-w-xl space-y-14 lg:mx-0 lg:max-w-lg">
+        <div className="space-y-8 text-center lg:text-left">
+          <h2 className="text-balance text-3xl font-bold leading-tight tracking-tight text-gray-900 sm:text-5xl">
+            Czekam na Twoje Zapytanie
+          </h2>
+
+          <p className="text-balance text-lg leading-8">
+            Masz pytania, potrzebujesz wsparcia, lub chcesz dowiedzieć się
+            więcej?
+          </p>
+
+          <p className="text-balance text-base leading-8">
+            Skontaktuj się ze mną już dziś! Wypełnij formularz kontaktowy,
+            zadzwoń lub wyślij e-mail.
+          </p>
+
+          <p className="text-balance text-base leading-8">
+            Z przyjemnością udzielę Ci odpowiedzi i pomogę znaleźć najlepsze
+            rozwiązania. Oczekuję na Twoją wiadomość!
+          </p>
         </div>
 
-        <div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <div className="space-y-4">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-base">
-                        Imię i Nazwisko
-                      </FormLabel>
-                      <FormControl>
-                        <Input {...field} type="text" className="min-h-16" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-base">
-                        Numer telefonu
-                      </FormLabel>
-                      <FormControl>
-                        <Input {...field} type="tel" className="min-h-16" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-base">E-mail</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" className="min-h-16" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="message"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-base">Wiadomość</FormLabel>
-                      <FormControl>
-                        <Textarea {...field} rows={10} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <Button type="submit" className="w-full" disabled={loading}>
-                {loading ? 'Wysyłanie...' : 'Wyślij'}
-              </Button>
-            </form>
-          </Form>
+        <div className="mx-auto max-w-xs space-y-4 text-base leading-7 lg:mx-0">
+          <div className="flex gap-x-4">
+            <Drawer>
+              <DrawerTrigger className="w-full">
+                <div className="flex w-full items-center gap-x-4">
+                  <BuildingOffice2Icon
+                    className="h-7 w-6 flex-shrink-0"
+                    aria-hidden="true"
+                  />
+                  <p className="flex flex-col items-center gap-x-4 text-balance hover:underline lg:flex-row">
+                    <span className="text-nowrap">
+                      Ul. Borówkowa 32, Rotmanka
+                    </span>
+                    <span className="text-nowrap">k. Pruszcza Gdańskiego</span>
+                  </p>
+                </div>
+              </DrawerTrigger>
+              <DrawerContent aria-describedby="address-description">
+                <DrawerHeader>
+                  <DrawerTitle>
+                    <p className="flex items-center justify-center gap-x-2 text-balance">
+                      <BuildingOffice2Icon
+                        aria-hidden="true"
+                        className="h-7 w-6"
+                      />{' '}
+                      Ul. Borówkowa 32, Rotmanka k. Pruszcza Gdańskiego
+                    </p>
+                  </DrawerTitle>
+                  <DrawerDescription></DrawerDescription>
+                </DrawerHeader>
+
+                <div
+                  className="bg-white-700 mx-auto my-5 h-[480px] w-[98%]"
+                  data-vaul-no-drag
+                >
+                  <Map posix={[54.26946270547405, 18.61158210184981]} />
+                </div>
+              </DrawerContent>
+            </Drawer>
+          </div>
+          <div className="flex items-center">
+            <Link
+              href="tel:+48883937022"
+              target="_blank"
+              className={cn(
+                buttonVariants({ variant: 'link' }),
+                'group flex items-center gap-2 p-0 text-zinc-900',
+              )}
+            >
+              <PhoneIcon aria-hidden="true" className="h-7 w-6" />
+              +48 883 937 022
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <Link
+              href="mailto:kontakt@majagrebska.pl"
+              target="_blank"
+              className={cn(
+                buttonVariants({ variant: 'link' }),
+                'group flex items-center gap-2 p-0 text-zinc-900',
+              )}
+            >
+              <EnvelopeIcon aria-hidden="true" className="h-7 w-6" />
+              kontakt@majagrebska.pl
+            </Link>
+          </div>
         </div>
       </div>
-    </Container>
+
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base">Imię i Nazwisko</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="text" className="min-h-16" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base">Numer telefonu</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="tel" className="min-h-16" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base">E-mail</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="email" className="min-h-16" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base">Wiadomość</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} rows={10} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading ? 'Wysyłanie...' : 'Wyślij'}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
   )
 }
