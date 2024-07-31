83de96cac8ac9693f1303281368a7f26c5c3cc42
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-07-31 14:12:50 +0200

    style(pricing): enhance text alignment and layout for better readability
    - Adjusted text alignment for better readability on various screen sizes.
    - Updated headings and paragraph alignments in `pricing/homepage` component.

--- src/components/pricing/homepage.tsx
+++ src/components/pricing/homepage.tsx
@@ -7,10 +7,10 @@ export default function PricingSection() {
   return (
     <>
       <div className="mx-auto max-w-2xl sm:text-center">
-        <h2 className="text-balance text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-balance text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Prosty cennik, jasne korzyści
         </h2>
-        <p className="mt-6 text-balance text-lg leading-8 text-gray-600">
+        <p className="mt-6 text-balance text-center text-lg leading-8 text-gray-600">
           Niezależnie od tego, czy chodzi o wstępną konsultację, czy regularną
           terapię indywidualną, cena za pojedynczą sesję obejmuje wszystko,
           czego potrzebujesz dla skutecznej i troskliwej opieki.
@@ -18,8 +18,15 @@ export default function PricingSection() {
       </div>
       <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
         <div className="p-8 sm:p-10 lg:flex-auto">
-          <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-            Konsultacja psychologiczna / psychoterapia indywidualna
+          <h3 className="flex flex-col gap-x-3 text-balance text-center text-2xl font-bold tracking-tight text-gray-900 lg:flex-row lg:text-left">
+            <span>Konsultacja psychologiczna</span>
+
+            <span>
+              <span className="hidden w-full lg:inline">/</span>
+              <span className="w-full lg:hidden">oraz</span>
+            </span>
+
+            <span>psychoterapia indywidualna</span>
           </h3>
           <p className="mt-6 text-base leading-7 text-gray-600">
             Moje usługi są dostosowane do Twoich unikalnych potrzeb, zapewniając
